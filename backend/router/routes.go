6b4d3c9e20a47c1da5f2270edffa6bea17c5c53f
Abyanzhafran <zhafranjupleks5758@gmail.com>
2023-08-03 16:44:35 +0700

    feat: - add cors gin package - implement cors to allow all origin and all methods

--- backend/router/routes.go
+++ backend/router/routes.go
@@ -4,11 +4,21 @@ import (
 	"candle-backend/controllers"
 	"candle-backend/repository"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter(repo *repository.BookRepository) *gin.Engine {
 	router := gin.Default()
+
+	// Create a new CORS config
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"*"}                                       // You can set specific origins here instead of "*"
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"} // Specify the allowed HTTP methods
+	config.AllowHeaders = []string{"Origin", "Content-Type"}                  // Specify the allowed headers
+
+	// Use the CORS middleware with the provided config
+	router.Use(cors.New(config))
 
 	BookCtrl := controllers.NewBookController(repo)
 
