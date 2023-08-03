259d5817814f76426ee3a1a3cd9bb3e24fa88f74
Abyanzhafran <zhafranjupleks5758@gmail.com>
2023-08-03 13:32:57 +0700

    feat : book models - refactor data models - add ImageFile data for handling file image, but it's not included in auto migrate to gorm rdbms - add form-data binding, it's just temporary, it should be only json or only form-data

--- backend/models/book.go
+++ backend/models/book.go
@@ -1,11 +1,14 @@
 package models
 
+import "mime/multipart"
+
 type Book struct {
-	ID          string `json:"id" gorm:"primary_key"`
-	Title       string `json:"title"`
-	Author      string `json:"author"`
-	Description string `json:"description"`
-	PublishDate string `json:"publishdate"`
-	ImageUrl    string `json:"imageurl"`
-	Price       uint64 `json:"price"`
+	ID          string `json:"id" form:"id" gorm:"primary_key"`
+	Title       string `json:"title" form:"title"`
+	Author      string `json:"author" form:"author"`
+	Description string `json:"description" form:"description"`
+	PublishDate string `json:"publishdate" form:"publishdate"`
+	ImageUrl    string `json:"imageurl" form:"imageurl"`
+	Price       uint64 `json:"price" form:"price"`
+	ImageFile 	*multipart.FileHeader `form:"imagefile" binding:"required" gorm:"-"`
 }
