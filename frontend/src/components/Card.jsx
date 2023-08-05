5c0f4e1796b1982dac73e030d9f19e1ea424e6ec
Abyanzhafran <zhafranjupleks5758@gmail.com>
2023-08-05 18:51:48 +0700

    fix : homepage and card size

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -21,10 +21,12 @@ export default function Card() {
       {booksData.map((obj) => (
         <div className="card w-[190px] bg-gray-100 shadow-sm">
           <figure className="w-[190px]">
-            <img src={obj.imageurl} />
+            <img src={obj.imageurl} className="h-48" />
           </figure>
-          <div className="card-body p-4">
-            <h2 className="text-lg font-semibold text-gray-700">{obj.title}</h2>
+          <div className="card-body p-4 h-44">
+            <h2 className="text-lg font-semibold text-gray-700 line-clamp-2">
+              {obj.title}
+            </h2>
             <p className="line-clamp-2 text-gray-700">{obj.description}</p>
             <div className="card-actions justify-start">
               <div className="text-md font-bold text-gray-700">
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Card from "../components/Card";
 
 export default function Home() {
   return (
-    <div className="w-full h-auto flex flex-wrap gap-6 items-start justify-center px-6 pb-10 pt-20 bg-gray-200">
+    <div className="w-full h-full flex flex-wrap gap-6 items-start justify-center px-6 pb-10 pt-20 bg-gray-200">
       <Card />
     </div>
   );
