17ffb3437b191a35f6c36bfb8f813c04dec8ed01
Abyanzhafran <zhafranjupleks5758@gmail.com>
2023-08-05 16:25:50 +0700

    feat : integrate home page card with API

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,23 +1,39 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export default function Card() {
+  const [booksData, setBooksData] = useState([]);
+  const api_url = import.meta.env.VITE_API_URL;
+
+  useEffect(() => {
+    axios
+      .get(`${api_url}/books`)
+      .then((response) => {
+        setBooksData(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <div className="card w-[190px] bg-gray-100 shadow-sm">
-      <figure className="w-[190px]">
-        <img
-          src="http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg"
-          alt="Movie"
-        />
-      </figure>
-      <div className="card-body p-4">
-        <h2 className="text-lg font-semibold text-gray-700">
-          Classical Muthology
-        </h2>
-        <p className="line-clamp-2 text-gray-700">
-          If a dog chews shoes whose shoes does he choose?
-        </p>
-        <div className="card-actions justify-start">
-          <div className="text-md font-bold text-gray-700">Rp. 100.000</div>
+    <>
+      {booksData.map((obj) => (
+        <div className="card w-[190px] bg-gray-100 shadow-sm">
+          <figure className="w-[190px]">
+            <img src={obj.imageurl} />
+          </figure>
+          <div className="card-body p-4">
+            <h2 className="text-lg font-semibold text-gray-700">{obj.title}</h2>
+            <p className="line-clamp-2 text-gray-700">{obj.description}</p>
+            <div className="card-actions justify-start">
+              <div className="text-md font-bold text-gray-700">
+                Rp. {obj.price}
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      ))}
+    </>
   );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,13 +4,6 @@ export default function Home() {
   return (
     <div className="w-full h-auto flex flex-wrap gap-6 items-start justify-center px-6 pb-10 pt-20 bg-gray-200">
       <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
     </div>
   );
 }
