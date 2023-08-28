61d25e82d299c88bd91f39d3887dd40316ca0ea9
Abyanzhafran <zhafranjupleks5758@gmail.com>
2023-08-28 08:15:31 +0700

    refactor : move database.go to app folder, delete config.go, the database connection config only from database.go file

---
+++ backend/app/database.go
@@ -0,0 +1,34 @@
+package app
+
+import (
+	"candle-backend/models"
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func NewDB() *gorm.DB {
+	Username := "root"
+	Password := ""
+	Host := "localhost"
+	Port := 3306
+	Name := "book_db"
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		Username, Password, Host, Port, Name)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
+
+	// auto-migrate model book
+	db.AutoMigrate(&models.Book{})
+	if err != nil {
+		log.Fatalf("failed to auto migrate: %v", err)
+	}
+
+	return db
+}
