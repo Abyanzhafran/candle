b32ab593ac8f9806c07617da218eaecd6b384360
Abyanzhafran <zhafranjupleks5758@gmail.com>
2023-08-29 23:35:21 +0700

    backend branch init commit

--- frontend/src/pages/About.jsx
+++
@@ -1,7 +0,0 @@
-export default function About() {
-  return (
-    <div className="flex items-center justify-center w-full h-screen">
-      <span className="text-2xl font-bold">About page</span>
-    </div>
-  )
-}
