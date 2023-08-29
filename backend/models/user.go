fea5681b0d86e8d9db007688ce4875fbb4242aaf
Abyanzhafran <zhafranjupleks5758@gmail.com>
2023-08-29 23:27:04 +0700

    frontend branch init commit

--- backend/models/user.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type User struct {
-	ID       string `json:"id" form:"id" gorm:"primary_key"`
-	Username string `json:"username" form:"username" gorm:"type:varchar(20)"`
-	Password string `json:"password" form:"password" gorm:"type:varchar(20)"`
-}
