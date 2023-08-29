1a3fefda4b0fe680fd17e377fc6e522333ea57d7
Abyanzhafran <zhafranjupleks5758@gmail.com>
2023-08-29 22:22:28 +0700

    add joho/godotenv library and it's configuration

--- backend/app/database.go
+++ backend/app/database.go
@@ -4,19 +4,25 @@ import (
 	"candle-backend/models"
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func NewDB() *gorm.DB {
-	Username := "root"
-	Password := ""
-	Host := "localhost"
-	Port := 3306
-	Name := "book_db"
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+	Username := os.Getenv("DBUSERNAME")
+	Password := os.Getenv("DBPASSWORD")
+	Host := os.Getenv("DBHOST")
+	Port := os.Getenv("DBPORT")
+	Name := os.Getenv("DBNAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		Username, Password, Host, Port, Name)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
