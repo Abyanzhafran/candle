a01967016067043647a6b1951ec47a0833af7a9c
Abyanzhafran <zhafranjupleks5758@gmail.com>
2023-08-29 23:21:32 +0700

    remove authorization in book_router

--- backend/router/book_router.go
+++ backend/router/book_router.go
@@ -3,7 +3,6 @@ package router
 import (
 	"candle-backend/app"
 	"candle-backend/controllers"
-	"candle-backend/middlewares"
 	"candle-backend/repository"
 
 	"github.com/gin-gonic/gin"
@@ -12,19 +11,13 @@ import (
 func BookRouter(router *gin.Engine) {
 	db := app.NewDB()
 
-	userRepo := repository.NewUserRepository(db)
 	bookRepo := repository.NewBookRepository(db)
 
 	BookCtrl := controllers.NewBookController(bookRepo)
 
-	bookRouter := router.Group("/admin", middlewares.AuthMiddleware(userRepo))
-
-	bookRouter.GET("/books", BookCtrl.FindAll)
-	// without authorization
-	router.GET("/:id", BookCtrl.GetBookByID)
-
-	// with authorization
-	bookRouter.POST("", BookCtrl.AddBook)
-	bookRouter.PUT("/:id", BookCtrl.EditBook)
-	bookRouter.DELETE("/:id", BookCtrl.DeleteBook)
+	router.GET("/books", BookCtrl.FindAll)
+	router.GET("/books/:id", BookCtrl.GetBookByID)
+	router.POST("/books", BookCtrl.AddBook)
+	router.PUT("/books/:id", BookCtrl.EditBook)
+	router.DELETE("/books/:id", BookCtrl.DeleteBook)
 }
