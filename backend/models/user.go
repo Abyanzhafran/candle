4ae12a2a72e476bf9333367ea2b65bdd0aa29775
Abyanzhafran <zhafranjupleks5758@gmail.com>
2023-08-31 15:57:39 +0700

    add UserRole data model in user entity

--- backend/models/user.go
+++ backend/models/user.go
@@ -4,4 +4,5 @@ type User struct {
 	ID       string `json:"id" form:"id" gorm:"primary_key"`
 	Username string `json:"username" form:"username" gorm:"type:varchar(20)"`
 	Password string `json:"password" form:"password" gorm:"type:varchar(20)"`
+	UserRole string `json:"userrole" form:"userrole" gorm:"type:varchar(20)"`
 }
